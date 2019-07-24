a7fcfb0f77ee2630dbe3311c7f6b6f66faac934b
slaveykov <slaweikow1997@abv.bg>
2019-07-24 22:28:53 +0300

    update

--- app/SaveWordsToDatabase.php
+++ app/SaveWordsToDatabase.php
@@ -23,6 +23,9 @@ class SaveWordsToDatabase
                 $findWord->word_index2 = $this->getWordIndex2($word);
                 $findWord->word_index3 = $this->getWordIndex3($word);
                 $findWord->word_index4 = $this->getWordIndex4($word);
+                $findWord->word_index5 = $this->getWordIndex5($word);
+                $findWord->word_index6 = $this->getWordIndex6($word);
+                $findWord->word_index7 = $this->getWordIndex7($word);
                 
                 $findWord->save();
                 
@@ -37,28 +40,50 @@ class SaveWordsToDatabase
         }
     }
     
+    
     public function getWordIndex1($word) {
+        
+        $lastSixLetters = mb_substr($word, -6);
+        
+        return $lastSixLetters;
+    }
+    
+    public function getWordIndex2($word) {
+        
+        $lastFiveLetters = mb_substr($word, -5);
+        
+        return $lastFiveLetters;
+    }
+    
+    public function getWordIndex3($word) {
         
         $lastForLetters = mb_substr($word, -4);
         
         return $lastForLetters;
     }
     
-    public function getWordIndex2($word) {
+    public function getWordIndex4($word) {
         
         $lastThreeLetters = mb_substr($word, -3);
         
         return $lastThreeLetters;
     }
     
-    public function getWordIndex3($word) {
+    public function getWordIndex5($word) {
+        
+        $firstFiveLetters = mb_substr($word, 0, 5);
+        
+        return $firstFiveLetters;
+    }
+    
+    public function getWordIndex6($word) {
         
         $firstFourLetters = mb_substr($word, 0, 4);
         
         return $firstFourLetters;
     }
     
-    public function getWordIndex4($word) {
+    public function getWordIndex7($word) {
         
         $firstThreeLetters = mb_substr($word, 0, 3);
         
